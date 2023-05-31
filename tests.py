c3e8e498462b60aa14650fa45831ea8278880e8f
Eric McKinney <ericmckinney.md@gmail.com>
2023-05-31 15:38:49 -0400

    Created and passed first csv parsing test

--- tests.py
+++ tests.py
@@ -1,9 +1,22 @@
 import unittest
+import main
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_parse_csv(self):
+        parsed_csv: list[main.Row] = main.parse_csv("example.csv", 0)
+        expected_parsed_csv: list[main.Row] = [
+            {"Name": "John Smith", "Date of birth": "3/24/1989", "Social Security Number": "123456",
+             "Employment Status": "Employed", "Favorite Color": "Red"},
+            {"Name": "Joe Bob", "Date of birth": "1/23/4567", "Social Security Number": "987654321",
+             "Employment Status": "Unemployed", "Favorite Color": "Orange"},
+            {"Name": "Emily Wayne", "Date of birth": "5/31/2000", "Social Security Number": "1234321",
+             "Employment Status": "Unknown", "Favorite Color": "Yellow"},
+            {"Name": "First Last", "Date of birth": "1/1/1970", "Social Security Number": "234111",
+             "Employment Status": "Unknown", "Favorite Color": "Green"}
+        ]
+
+        self.assertEqual(parsed_csv, expected_parsed_csv)
 
 
 if __name__ == '__main__':
