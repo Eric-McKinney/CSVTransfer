e40356aecfcf87b5f104cd82b97d94332d09576b
Eric McKinney <ericmckinney.md@gmail.com>
2023-06-06 16:17:15 -0400

    Fixed tests according to better parsing

--- tests.py
+++ tests.py
@@ -29,40 +29,19 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual(expected_parsed_csv, parsed_csv)
 
-    def test_normalize_line(self):
-        line1: str = "one,two,three"
-        line2: str = "one,\"two, three\""
-        line3: str = "\"one, two\",three"
-        line4: str = "one,\"two, three\",four"
-
-        normalized_line1: str = main.normalize_line_with_quotes(line1)
-        normalized_line2: str = main.normalize_line_with_quotes(line2)
-        normalized_line3: str = main.normalize_line_with_quotes(line3)
-        normalized_line4: str = main.normalize_line_with_quotes(line4)
-
-        expected_normalized1: str = "one,two,three"
-        expected_normalized2: str = "one,two three"
-        expected_normalized3: str = "one two,three"
-        expected_normalized4: str = "one,two three,four"
-
-        self.assertEqual(expected_normalized1, normalized_line1)
-        self.assertEqual(expected_normalized2, normalized_line2)
-        self.assertEqual(expected_normalized3, normalized_line3)
-        self.assertEqual(expected_normalized4, normalized_line4)
-
     def test_parse_csv3(self):
         parsed_csv: list[main.Row] = main.parse_csv("example3.csv", 1, [0])
         expected_parsed_csv: list[main.Row] = [
-            {"social security": "", "d.o.b": "", "last name first name": "Bob Joe", "employment status": "employed",
+            {"social security": "", "d.o.b": "", "last name, first name": "Bob, Joe", "employment status": "employed",
              "favorite color": "Teal", "hobbies": "Tennis", "comments": ""},
-            {"social security": "1234321", "d.o.b": "", "last name first name": "Wayne Emily", "employment status": "",
+            {"social security": "1234321", "d.o.b": "", "last name, first name": "Wayne, Emily", "employment status": "",
              "favorite color": "Red", "hobbies": "", "comments": "No comment"},
-            {"social security": "234111", "d.o.b": "1/1/1970", "last name first name": "Last First",
+            {"social security": "234111", "d.o.b": "1/1/1970", "last name, first name": "Last, First",
              "employment status": "", "favorite color": "Green", "hobbies": "Deliberate misinformation",
              "comments": "Mr. Unix Epoch"},
-            {"social security": "", "d.o.b": "", "last name first name": "", "employment status": "",
+            {"social security": "", "d.o.b": "", "last name, first name": "", "employment status": "",
              "favorite color": "", "hobbies": "", "comments": ""},
-            {"social security": "565", "d.o.b": "", "last name first name": "", "employment status": "employed",
+            {"social security": "565", "d.o.b": "", "last name, first name": "", "employment status": "employed",
              "favorite color": "Royal purple", "hobbies": "No hobby", "comments": ""}
         ]
 
@@ -71,12 +50,12 @@ class MyTestCase(unittest.TestCase):
     def test_parse_csv4(self):
         parsed_csv: list[main.Row] = main.parse_csv("example3.csv", 1, [0, 2, 5])
         expected_parsed_csv: list[main.Row] = [
-            {"social security": "1234321", "d.o.b": "", "last name first name": "Wayne Emily", "employment status": "",
-             "favorite color": "Red", "hobbies": "", "comments": "No comment"},
-            {"social security": "234111", "d.o.b": "1/1/1970", "last name first name": "Last First",
+            {"social security": "1234321", "d.o.b": "", "last name, first name": "Wayne, Emily",
+             "employment status": "", "favorite color": "Red", "hobbies": "", "comments": "No comment"},
+            {"social security": "234111", "d.o.b": "1/1/1970", "last name, first name": "Last, First",
              "employment status": "", "favorite color": "Green", "hobbies": "Deliberate misinformation",
              "comments": "Mr. Unix Epoch"},
-            {"social security": "565", "d.o.b": "", "last name first name": "", "employment status": "employed",
+            {"social security": "565", "d.o.b": "", "last name, first name": "", "employment status": "employed",
              "favorite color": "Royal purple", "hobbies": "No hobby", "comments": ""}
         ]
 
