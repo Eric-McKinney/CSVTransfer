d9b3bcb7d071608469fee8b728e91121522316f2
Eric McKinney <ericmckinney.md@gmail.com>
2023-08-02 08:16:26 -0400

    Added comment skeleton

---
+++ config_gen.py
@@ -0,0 +1,24 @@
+
+def main():
+    # series of prompts that ask
+    # - config file name
+    # - number of sources
+    # - name of each source + path of csv
+    # for each source
+    # - target columns
+    # - new names for said columns
+    # - columns to match by
+    # - new names for said columns
+    # - header row num
+    # - ignored row nums
+    # - rules?
+    # then
+    # - field rules?
+    # - output file name
+    # - unmatched output file name
+    # - output dialect
+    pass
+
+
+if __name__ == "__main__":
+    main()
