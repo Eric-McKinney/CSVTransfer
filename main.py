8dbd405076866ef57b6c7ae5c4e85fb87e98fe2b
Eric McKinney <ericmckinney.md@gmail.com>
2023-08-08 11:57:15 -0400

    Implemented basic constructor for Config class

--- main.py
+++ main.py
@@ -18,11 +18,13 @@ file paths given in the config file are valid. To run this script enter "python3
 your environment.
 """
 import configparser
+import config_gen
 import csv
 import os
 import re
 import sys
 from typing import Iterable
+from pathlib import Path
 
 # Custom type aliases for clarity
 Header = str
@@ -75,8 +77,20 @@ class Config2:  # temp name which will take place of the Config type alias event
     all_headers: list[Header]
     # etc idk...
 
-    def __init__(self):
-        pass
+    def __init__(self, config_file_name: str):
+        if not Path(config_file_name).is_file():
+            print(f"\"{config_file_name}\" not found. You may need to change CONFIG_FILE_NAME in {sys.argv[0]}")
+            gen_cfg: bool = input("Would you like to generate a config file (y/N)? ").lower() in ["y", "yes"]
+
+            if gen_cfg:
+                config_gen.main()
+                print("\nMake sure CONFIG_FILE_NAME has the name of your new config file before rerunning")
+
+            exit(1)
+
+        self.config = configparser.ConfigParser(allow_no_value=True)
+        self.config.optionxform = str
+        self.config.read(config_file_name)
 
     def validate(self):
         pass
